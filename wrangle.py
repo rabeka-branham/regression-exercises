4d071fd689869c6fced20c4158aabf88f94dd8e7
Rabeka Branham <rabeka.june@gmail.com>
2023-12-13 22:08:02 -0600

    updated after review

--- wrangle.py
+++ wrangle.py
@@ -5,43 +5,55 @@ from sklearn.model_selection import train_test_split
 
 def check_file_exists(filename, query, url):
     if os.path.exists(filename):
-        # print('File exists - reading CSV file')
         df = pd.read_csv(filename, index_col=0)
+        
     else:
-        # print('File does not exist - creating CSV file')
         df = pd.read_sql(query,url)
         df.to_csv(filename)
+        
     return df
 
 def acquire_zillow():
     filename = 'zillow.csv'
     url = get_db_url('zillow')
     query = """
-   select bedroomcnt, 
-        bathroomcnt,
-        calculatedfinishedsquarefeet,
-        taxvaluedollarcnt,
-        yearbuilt,
-        taxamount, 
-        fips 
-    from properties_2017 as p
-    join propertylandusetype as l
-        on p.propertylandusetypeid = l.propertylandusetypeid
-    where propertylandusedesc in ('Single Family Residential')
-    """
+           select bedroomcnt, 
+                bathroomcnt,
+                calculatedfinishedsquarefeet,
+                taxvaluedollarcnt,
+                yearbuilt,
+                taxamount, 
+                fips 
+            from properties_2017
+            where propertylandusetypeid = 261
+            """
 
     df = check_file_exists(filename,query,url)
-    
     return df
 
 def prep_zillow(df):
     df = df.dropna()
     df = df.rename(columns={'bedroomcnt': 'bed_count', 
-                   'bathroomcnt' : 'bath_count',
-                   'calculatedfinishedsquarefeet' : 'sq_ft',
-                   'taxvaluedollarcnt': 'property_value',
-                   'yearbuilt': 'year_built',
-                   'taxamount': 'tax_amount'})
+                           'bathroomcnt' : 'bath_count',
+                           'calculatedfinishedsquarefeet' : 'area',
+                           'taxvaluedollarcnt': 'property_value',
+                           'yearbuilt': 'year_built',
+                           'taxamount': 'tax_amount',
+                           'fips': 'county'})
+    
+    make_int = []
+    for col in df.columns:
+        has_non_zero_decimal = df[col].apply(lambda x: x % 1 != 0)
+        if has_non_zero_decimal.sum() == 0:
+            make_int.append(col)
+        else:
+            continue
+        
+    for col in make_int:
+        df[col] = df[col].astype(int)
+        
+    df.county = df.county.map({6037:'Los Angeles',6059:'Orange',6111:'Ventura'})
+    
     df = df.reset_index().drop(columns='index')
     
     return df
@@ -58,9 +70,7 @@ def split_data(dataframe):
     return train, validate, test
 
 def wrangle_zillow():
-    df = acquire_zillow()
-    df = prep_zillow(df)
-    train, validate, test = split_data(df)
+    train, validate, test = split_data(prep_zillow(acquire_zillow()))
     
     return train, validate, test
     
